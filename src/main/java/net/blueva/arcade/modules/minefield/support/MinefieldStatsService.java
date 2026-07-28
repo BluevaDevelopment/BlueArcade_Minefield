a40bf7f40f8e2efdd8be41cecb3b535516dc8f8c
Whiron <dnlblasco@gmail.com>
2026-07-28 23:15:55 +0200

    fix: stop writing the core-owned global wins stat

--- src/main/java/net/blueva/arcade/modules/minefield/support/MinefieldStatsService.java
+++ src/main/java/net/blueva/arcade/modules/minefield/support/MinefieldStatsService.java
@@ -56,7 +56,6 @@ public class MinefieldStatsService {
         }
 
         statsAPI.addModuleStat(player, moduleInfo.getId(), "wins", 1);
-        statsAPI.addGlobalStat(player, "wins", 1);
     }
 
     public void recordFinishLine(Player player) {
